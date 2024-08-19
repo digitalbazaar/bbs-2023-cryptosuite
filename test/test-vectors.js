73ba791e1975da8b336260c196008a5a0503c403
Dave Longley <dlongley@digitalbazaar.com>
2024-08-19 18:52:23 -0400

    Use `@digitalbazaar/bls12-381-multikey@2` and update test vectors.

--- test/test-vectors.js
+++ test/test-vectors.js
@@ -41,6 +41,7 @@ export const signedSDBase = {
   "type": [
     "VerifiableCredential"
   ],
+  "issuer": "https://vc.example/windsurf/racecommittee",
   "credentialSubject": {
     "sailNumber": "Earth101",
     "sails": [
@@ -84,7 +85,7 @@ export const signedSDBase = {
     "created": "2023-08-15T23:36:38Z",
     "verificationMethod": "did:key:zUC7DerdEmfZ8f4pFajXgGwJoMkV1ofMTmEG5UoNvnWiPiLuGKNeqgRpLH2TV4Xe5mJ2cXV76gRN7LFQwapF1VFu6x2yrr5ci1mXqC1WNUrnHnLgvfZfMH7h6xP6qsf9EKRQrPQ#zUC7DerdEmfZ8f4pFajXgGwJoMkV1ofMTmEG5UoNvnWiPiLuGKNeqgRpLH2TV4Xe5mJ2cXV76gRN7LFQwapF1VFu6x2yrr5ci1mXqC1WNUrnHnLgvfZfMH7h6xP6qsf9EKRQrPQ",
     "proofPurpose": "assertionMethod",
-    "proofValue": "u2V0ChdhAWFCKn4dlQY9B3YMaAmVMTmD8bAcksNKyWcHAUF3Oh5chgdWwBLehP7jXMgyinO5h0H4D3s0RxzgDksDTqyXaI3B8BoqHsUQp7Z-Fjis2xIp_DthAWEA6W78l002QsYw1zSNXvmpvQjAelPyeUvd-k7dzxWFL36jkBckYFrsZd9sl_H-Bd6z6zLefgh7YBaRV5oVfM0te2EBYYKTvGvo9pXVJbxIrm3i4wkdhUxqKCTIGrnxFuAdZwWi6T3omD5wzZ7bAGbRneEEQSxBmXtvnC6Pr59nPv_v3HrAW9wq_uxYzF_NyaX3GPv0h_FV2T2OSao8C6uoyWiqIj9hAWCAAESIzRFVmd4iZqrvM3e7_ABEiM0RVZneImaq7zN3u_4R4HS9jcmVkZW50aWFsU3ViamVjdC9zYWlsTnVtYmVyeBovY3JlZGVudGlhbFN1YmplY3Qvc2FpbHMvMXggL2NyZWRlbnRpYWxTdWJqZWN0L2JvYXJkcy8wL3llYXJ4Gi9jcmVkZW50aWFsU3ViamVjdC9zYWlscy8y"
+    "proofValue": "u2V0ChVhQgzH1WtRY_lwyJCCyy4BvmiDqayuKKdUXEAJtcazl2ggAZLSIgY78daQ5UlvQMUUIIqajMtp4GSbhk2C5AWZDESTvzz0GD7x1DGEixxTAf3FYQDpbvyXTTZCxjDXNI1e-am9CMB6U_J5S936Tt3PFYUvfVV3gX4mIF-MTAbrBh9DD_ysD4svbSttNVowX3pYfmhhYYKTvGvo9pXVJbxIrm3i4wkdhUxqKCTIGrnxFuAdZwWi6T3omD5wzZ7bAGbRneEEQSxBmXtvnC6Pr59nPv_v3HrAW9wq_uxYzF_NyaX3GPv0h_FV2T2OSao8C6uoyWiqIj1ggABEiM0RVZneImaq7zN3u_wARIjNEVWZ3iJmqu8zd7v-FZy9pc3N1ZXJ4HS9jcmVkZW50aWFsU3ViamVjdC9zYWlsTnVtYmVyeBovY3JlZGVudGlhbFN1YmplY3Qvc2FpbHMvMXggL2NyZWRlbnRpYWxTdWJqZWN0L2JvYXJkcy8wL3llYXJ4Gi9jcmVkZW50aWFsU3ViamVjdC9zYWlscy8y"
   }
 };
 
@@ -98,6 +99,7 @@ export const derivedRevealDocument = {
   "type": [
     "VerifiableCredential"
   ],
+  "issuer": "https://vc.example/windsurf/racecommittee",
   "credentialSubject": {
     "sailNumber": "Earth101",
     "sails": [
@@ -131,7 +133,7 @@ export const derivedRevealDocument = {
     "created": "2023-08-15T23:36:38Z",
     "verificationMethod": "did:key:zUC7DerdEmfZ8f4pFajXgGwJoMkV1ofMTmEG5UoNvnWiPiLuGKNeqgRpLH2TV4Xe5mJ2cXV76gRN7LFQwapF1VFu6x2yrr5ci1mXqC1WNUrnHnLgvfZfMH7h6xP6qsf9EKRQrPQ#zUC7DerdEmfZ8f4pFajXgGwJoMkV1ofMTmEG5UoNvnWiPiLuGKNeqgRpLH2TV4Xe5mJ2cXV76gRN7LFQwapF1VFu6x2yrr5ci1mXqC1WNUrnHnLgvfZfMH7h6xP6qsf9EKRQrPQ",
     "proofPurpose": "assertionMethod",
-    "proofValue": "u2V0DhdhAWQIQpfdWUrAhGTOffbGNFoN9ABqtEpgN2K2DsTEEEWkhb8LbeDm4Ac9K-Pw3ZkS6F6Dfk6NoLdOiElhEB093vHndQFWSkS6qWE1Y1fCwl39YPK3hnjWVI7T4o_Cm7ky9TFfTktJ-g3Czll_YIeyRYkdJ5i4CFBR0IlWvJ9TqoHNezMHG8ZeOgw5g_hnTMzKoZDkSMeRr8jVjwz84QpicLEz00SEFYUqsx_5vul1gy_afkJlVqIHDHuDw8F8xIdoO68mrdIDQAidoTfz43uAJetv8S2odynGrMMvnTh9UQHzME3E9UmtfN_YWvn--8RTzFqWwFzdThg7_iMnMaDRX5onQ6sVa4ihmDda8VZTos_6YWbtK5Dhe4wFqTwc9-uXE5jvTJ1U3a66V_QcFkWhYHJ1YPxMATUtHAxEwIIRowrUkBNYRVYE-4JU9tEl03lTEmFy_K4ucM-_UN1LrwzyK9toa_CcMfOgm8ZUlv2iRVb4JKwcjQ8tUlXBEUUms9J0DKEiibjzgBhFjSg01r24-j2Ibv06vCyH7mk9bLkchxQn4ZImntT8i7hwi4QSxrd9at9AdJa4qeV6Q95Nrckmj8QvmOVx81JY7ok1Co8VFmMHrFExVjfZhTQ1PVG_jK_Wae-qNzC5ITr-hx0uNw2AuWyjs81XeDDCyilODKQIMOpLX3_j71E_0SZcWppvVFlOQY0YApgACAQQCBwMGBAUFAI0AAQIFBgcICQoLDQ4PhgMECAsMDdhARBEzd6o"
+    "proofValue": "u2V0DhVkCEJgxugaFJpT7ROtWzZ9mWBMw2Uk2caOtXtKGEMJVDFv9psrafLrzfprwyHOk7GgTv4V9U5VDvEW6E0n-MjO0RvbEYZDECqhFbZgxLtdTXDAD46d691Ltb37hYt9OOKJorYfMWhD_ONzGYzgQ4IrFqA2s_m597DymX7HauNGw2iK48mBAI4xwC4MQ3pLJwuwRiy3msMzccvvdMynM97xymCnoSS0KeW9uCRMYhPb90N-AKNXvjwXZqpgXhyWYxWQhUm2-XbQFhs0rg6RUZS9xY35XkXq9IvRbtn1I_OvfVGRnGuwuhF-H-HwdDrk02z-54jENSD1nEQtfZBJ4J4iOjNklnqePZoMYTKTnGEW4A9k6NVT0V3cW-Tm9NvJut0B3G9XDUkfvSrwrDnAXIabo7fYqY686Ay34lc3gbQsVyowadQckkRj50Jb8xaP5o57BqHDvYZ76avYf2Tt0uCskMX3vWfmB_I7CtWM9jrhxGxCFUre250hkhQP-zfUqwKduyokwY2EmLMR2e7uE6QTRp1I7wZ1nvFAceJSWFr72VHCwZ_gXWdmin5wndcCIikYXtXAY7OER5izYNltHg_vlO87IRr9yS93cGW_O0FxZw167c1rqmoPw5SM825-7j9LjsAfuf2nK_DfEmT3fx0fXeTtI6kghMVS0WSYMKdpt1B3pU5ozUoVa-jmLK6_UfQfXZaYAAgEEAgMDBwQGBQCOAAECBQYICQoODxAREhOGAwQFCAkKRBEzd6o"
   }
 };
 /* eslint-enable quotes */
